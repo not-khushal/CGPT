7e096c39510caef81f5401abde68e220b256d990
not-khushal <138289978+not-khushal@users.noreply.github.com>
2025-04-01 19:22:28 +0530

    Add files via upload

---
+++ assignment8/Q9.java
@@ -0,0 +1,19 @@
+// Working with Multi-Dimensional Arrays. Write a Java program to create a 2D Array and 
+// Copy the 2D Array into a single dimension array
+// Hint => 
+// Take user input for rows and columns, create a 2D array (Matrix), and take the user input 
+// Copy the elements of the matrix to a 1D array. For this create a 1D array of size rows*columns as in 
+// int[] array = new int[rows * columns];
+// Define the index variable and Loop through the 2D array. 
+// Copy every element of the 2D array into the 1D array and increment the index
+// Note: For looping through the 2D array, you will need Nested for loop, 
+// Outer for loop for rows, and the inner for loops to access each element
+
+
+package assignment8;
+
+public class Q9 {
+    public static void main(String[] args) {
+        
+    }
+}
